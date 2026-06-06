ac450b74b3cf863abc99e6a7fe94dd71c604c4f5
Vitaliy Zhyryk <vitaliy.zhyryk@gmail.com>
2026-06-06 13:16:17 +0200

    Update views.sql

--- topic-10-views/views.sql
+++ topic-10-views/views.sql
@@ -117,10 +117,16 @@ SELECT
         FROM borrowings br
         WHERE br.member_id = m.member_id
           AND br.returned_at IS NULL
-    ) AS active_books_count
-FROM members m;
-
-
+          AND br.due_date < CURRENT_DATE
+    ) AS overdue_count
+FROM members m
+WHERE EXISTS (
+    SELECT 1
+    FROM borrowings br
+    WHERE br.member_id = m.member_id
+      AND br.returned_at IS NULL
+      AND br.due_date < CURRENT_DATE
+);
 
 -- ================================================================
 -- VIEW BASED ON ANOTHER VIEW
